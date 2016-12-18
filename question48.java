1cef61d484eafc81ca22823e41ffe3af7e4b8c7c
Huaye Li <lih6@cs.washington.edu>
2016-12-17 18:58:43 -0800

    question48

---
+++ question48.java
@@ -0,0 +1,29 @@
+public class Solution {
+    public void rotate(int[][] matrix) {
+        int length = matrix.length;
+        
+        if (length <= 1) {
+            return;
+        }
+        
+        int numOfLayers = length / 2; // Smaller half
+        int numOfPairs = length - 1;
+        
+        
+        for (int layer = 0; layer < numOfLayers; layer++) {
+            for (int pair = layer; pair < numOfPairs; pair++) {
+                int first = matrix[layer][pair];
+                int second = matrix[pair][numOfPairs];
+                int third = matrix[numOfPairs][length - 1 - pair];
+                int fourth = matrix[length - 1 - pair][layer];
+                
+                matrix[layer][pair] = fourth;
+                matrix[pair][numOfPairs] = first;
+                matrix[numOfPairs][length - 1 - pair] = second;
+                matrix[length - 1 - pair][layer] = third;
+                
+            }
+            numOfPairs--;
+        }
+    }
+}
